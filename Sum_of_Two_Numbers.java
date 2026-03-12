7eb81428a2c6a8b57bf45ddb784ab250395ab42a
NKDugar_55 <naveeneducation55@gmail.com>
2026-03-12 14:04:02 +0530

    SumOfTwoNumbers

---
+++ Sum_of_Two_Numbers.java
@@ -0,0 +1,10 @@
+public class Sum_of_Two_Numbers {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        
+        int sum = num1 + num2;
+        
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
